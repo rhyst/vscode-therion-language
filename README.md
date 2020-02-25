179c0309648854909c1bfb0d16dfd1ac0d98b895
Rhys Tyers <rhystyers1@gmail.com>
2020-02-25 15:54:19 +0000

    Add compile and compile watching commands

--- README.md
+++ README.md
@@ -6,7 +6,14 @@ Tools for the Therion cave surveying programme in VS Code.
 
 - Syntax Highlighting
 - Formatting
+- Useful commands and context menu items
 
-## Available Commands
+## Commands
 
-- `Project Manager: Create scrap template` Create a new scrap from the current `.th` file complete with stations and centreline, ready for drawing
+- `Therion: Create scrap template` Create a new scrap from the current `.th` file complete with stations and centreline, ready for drawing
+- `Therion: Compile` Compile the current `.thconfig` file
+- `Therion: Watch` Compile the current `.thconfig` file and recompile if there are changes to it or any nested input or source
+
+## Menu items
+
+- `Open in Inkscape` Available when opening the context menu on a `.th2` in the file explore. Opens the file in Inkscape.
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,24 @@ import * as fs from "fs";
 import * as path from "path";
 import * as vscode from "vscode";
 import * as tmp from "tmp";
+import * as util from "util";
 import * as cp from "child_process";
+import * as chokidar from "chokidar";
 
-const indentCharacter = "\t";
+const exec = util.promisify(cp.exec);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const outputChannel = vscode.window.createOutputChannel("Therion");
+
+const getConfig = key => {
+  const config = vscode.workspace.getConfiguration("therion");
+  const keys = key.split(".");
+  if (keys.length === 1) return config.get(key);
+  if (keys.length === 2) return config.get(keys[0])[keys[1]];
+  return null;
+};
+
 const startTags = /^(scrap|survey|map|centreline|centerline|line|layout|lookup|def|begingroup)(\s|$)/;
 const endTages = /^(endscrap|endsurvey|endmap|endcentreline|endcenterline|endline|endlayout|endlookup|enddef(;)?|endgroup(;)?|endcode)(\s|$)/;
 const code = /^code\s/;
@@ -23,7 +38,9 @@ export function activate(context: vscode.ExtensionContext) {
 
         // Indendation
         if (endTages.test(text)) indent--;
-        const newText = `${indentCharacter.repeat(indent)}${text}`;
+        const newText = `${getConfig("format.indentCharacters").repeat(
+          indent
+        )}${text}`;
         if (startTags.test(text)) indent++;
         edits.push(vscode.TextEdit.replace(document.lineAt(i).range, newText));
 
@@ -86,63 +103,211 @@ endmap
 endline`;
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("therion.createScrapTemplate", () => {
-      const editor = vscode.window.activeTextEditor;
-      // Split up file path
-      const thFilePath = editor.document.fileName;
-      const thFolderPath = path.dirname(thFilePath);
-      const thFileName = path.basename(thFilePath);
-      const thFileExt = path.extname(thFilePath);
-      const name = thFileName.replace(thFileExt, "");
+    vscode.commands.registerCommand(
+      "therion.command.createScrapTemplate",
+      async () => {
+        const editor = vscode.window.activeTextEditor;
+        // Split up file path
+        const thFilePath = editor.document.fileName;
+        const thFolderPath = path.dirname(thFilePath);
+        const thFileName = path.basename(thFilePath);
+        const thFileExt = path.extname(thFilePath);
+        const name = thFileName.replace(thFileExt, "");
 
-      // Export XVI
-      const tmpConfig = tmp.fileSync();
-      const tmpXVI = tmp.fileSync();
-      fs.writeFileSync(tmpConfig.name, xviTemplate(thFilePath, tmpXVI.name));
-      cp.execSync(`therion ${tmpConfig.name}`);
+        // Export XVI
+        const tmpConfig = tmp.fileSync();
+        const tmpXVI = tmp.fileSync();
+        await writeFile(tmpConfig.name, xviTemplate(thFilePath, tmpXVI.name));
+        await compile(`${tmpConfig.name}`);
 
-      // Extract points and lines from XVI
-      const xviContents = fs.readFileSync(tmpXVI.name, "utf8");
-      const xviLines = xviContents.split("\n").reverse();
-      const points = [];
-      const lines = [];
-      xviLines.forEach(line => {
-        let match = line.match(/{\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(\d+)\s*}/);
-        if (match) {
-          const x = match[1];
-          const y = match[2];
-          const station = match[3];
-          points.push(pointTemplate(x, y, station));
-        }
-        match = line.match(
-          /{\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*}/
+        // Extract points and lines from XVI
+        const xviContents = await readFile(tmpXVI.name, "utf8");
+        const xviLines = xviContents.split("\n").reverse();
+        const points = [];
+        const lines = [];
+        xviLines.forEach(line => {
+          let match = line.match(/{\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(\d+)\s*}/);
+          if (match) {
+            const x = match[1];
+            const y = match[2];
+            const station = match[3];
+            points.push(pointTemplate(x, y, station));
+          }
+          match = line.match(
+            /{\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*}/
+          );
+          if (match) {
+            const x1 = match[1];
+            const y1 = match[2];
+            const x2 = match[3];
+            const y2 = match[4];
+            lines.push(lineTemplate(x1, y1, x2, y2));
+          }
+        });
+
+        // Write template scrap file
+        await writeFile(
+          path.join(thFolderPath, `${name}-p.th2`),
+          scrapTemplate(name, points.join("\n"), lines.join("\n"))
         );
-        if (match) {
-          const x1 = match[1];
-          const y1 = match[2];
-          const x2 = match[3];
-          const y2 = match[4];
-          lines.push(lineTemplate(x1, y1, x2, y2));
-        }
-      });
 
-      // Write template scrap file
-      fs.writeFileSync(
-        path.join(thFolderPath, `${name}-p.th2`),
-        scrapTemplate(name, points.join("\n"), lines.join("\n"))
-      );
-
-      // Insert scrap into map in document
-      for (let i = 0; i < editor.document.lineCount; i++) {
-        const text = editor.document.lineAt(i).text.trim();
-        if (/^cent(re|er)line/.test(text)) {
-          const centrelinePosition = editor.document.lineAt(i).range.start;
-          vscode.window.activeTextEditor.edit(editBuilder => {
-            editBuilder.insert(centrelinePosition, mapTemplate(name));
-          });
-          break;
+        // Insert scrap into map in document
+        for (let i = 0; i < editor.document.lineCount; i++) {
+          const text = editor.document.lineAt(i).text.trim();
+          if (/^cent(re|er)line/.test(text)) {
+            const centrelinePosition = editor.document.lineAt(i).range.start;
+            vscode.window.activeTextEditor.edit(editBuilder => {
+              editBuilder.insert(centrelinePosition, mapTemplate(name));
+            });
+            break;
+          }
         }
+
+        vscode.window.showInformationMessage(`Scrap created for ${name}.`);
+        outputChannel.appendLine(`Scrap created for ${name}.`);
       }
+    )
+  );
+
+  const compile = async filepath => {
+    const name = path.basename(filepath);
+    outputChannel.appendLine(`Compiling: ${name}`);
+    try {
+      await exec(`${getConfig("therionPath")} ${filepath}`);
+      outputChannel.appendLine(`Compiled: ${name}`);
+      vscode.window.showInformationMessage(`Compiled: ${name}`);
+    } catch (e) {
+      vscode.window.showErrorMessage(`Failed to compile: ${name}`);
+      outputChannel.appendLine(`Failed to compile: ${name}`);
+      outputChannel.appendLine(`stderr:`);
+      e.stderr
+        .toString()
+        .split("\n")
+        .map(line => outputChannel.appendLine(`\t${line.trim()}`));
+      outputChannel.appendLine(`stdout:`);
+      e.stdout
+        .toString()
+        .split("\n")
+        .map(line => outputChannel.appendLine(`\t${line.trim()}`));
+      outputChannel.show(true);
+    }
+  };
+
+  // Compile thconfig
+  context.subscriptions.push(
+    vscode.commands.registerCommand("therion.command.compile", () => {
+      const editor = vscode.window.activeTextEditor;
+      const thFilePath = editor.document.fileName;
+      compile(thFilePath);
+    })
+  );
+
+  // Watch thconfig
+  const watchStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    0
+  );
+  context.subscriptions.push(watchStatusBarItem);
+  watchStatusBarItem.command = "therion.command.stop-watch";
+  watchStatusBarItem.hide();
+
+  const watches = new Map();
+
+  const startWatch = (key, name, files) => {
+    const watcher = chokidar.watch(files);
+    watcher.on("change", path => {
+      outputChannel.appendLine(`Changed: ${path}`);
+      compile(key);
+    });
+    watches.set(key, { name, file: key, watcher });
+    updateWatchStatus();
+    vscode.window.showInformationMessage(`Started watching ${name}.`);
+    outputChannel.appendLine(`Started watching ${name}.`);
+    compile(key);
+  };
+
+  const stopWatch = key => {
+    const { name, watcher } = watches.get(key);
+    watcher.close();
+    watches.delete(key);
+    updateWatchStatus();
+    vscode.window.showInformationMessage(`Stopped watching ${name}.`);
+    outputChannel.appendLine(`Stopped watching ${name}.`);
+  };
+
+  const updateWatchStatus = () => {
+    if (watches.size === 0) {
+      watchStatusBarItem.hide();
+      return;
+    }
+    watchStatusBarItem.show();
+    if (watches.size === 1) {
+      watchStatusBarItem.text = `Therion watching: ${path.basename(
+        Array.from(watches.values())[0].name
+      )}`;
+    } else {
+      watchStatusBarItem.text = `Therion watching: ${watches.size} files`;
+    }
+  };
+
+  const inputReg = /\n\s*(?:input|source)\s+(\S+)/g;
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("therion.command.watch", async () => {
+      const file = vscode.window.activeTextEditor.document.fileName;
+      const name = path.basename(file);
+      if (watches.get(file)) {
+        vscode.window.showInformationMessage(
+          `${name} is already being watched.`
+        );
+        return;
+      }
+      const getInputs = async file => {
+        const text = await readFile(file, "utf8");
+        const inputs = Array.from(text.matchAll(inputReg)).map(i => {
+          const j = path.join(path.dirname(file), i[1].replace(/\"/g, ""));
+          return path.extname(j) ? j : `${j}.th`;
+        });
+        let files = [file];
+        for (const input of inputs) {
+          const moreFiles = await getInputs(input);
+          files = files.concat(...moreFiles);
+        }
+        return files;
+      };
+      const files = await getInputs(file);
+      startWatch(file, name, files);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("therion.command.stop-watch", e => {
+      const watchesArr = Array.from(watches.values());
+      if (watchesArr.length === 1) {
+        stopWatch(watchesArr[0].file);
+        return;
+      }
+      const quickPick = vscode.window.createQuickPick();
+      let selection = null;
+      quickPick.items = watchesArr.map(watch => ({
+        label: `Stop watching ${watch.name}`,
+        name: watch.name,
+        file: watch.file
+      }));
+      quickPick.title = "Choose a file to stop watching";
+      quickPick.onDidChangeSelection(s => (selection = s));
+      quickPick.onDidAccept(() => {
+        quickPick.hide();
+        stopWatch(selection[0].file);
+      });
+      quickPick.show();
+    })
+  );
+
+  // Open in Inkscape Context Command
+  context.subscriptions.push(
+    vscode.commands.registerCommand("therion.context.openInInkscape", file => {
+      cp.exec(`${getConfig("inkscapePath")} ${file.path}`);
     })
   );
 }
