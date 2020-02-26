e7ea1abdde40ba9b70203849954b0d65df5bff55
Rhys Tyers <5313660+rhyst@users.noreply.github.com>
2020-02-26 10:42:07 +0000

    Ensure formatting runs in all files, add more built-ins to highlighting

--- src/extension.ts
+++ src/extension.ts
@@ -27,38 +27,43 @@ const inlineCode = /\\/m;
 
 export function activate(context: vscode.ExtensionContext) {
   // Formatter
-  vscode.languages.registerDocumentFormattingEditProvider("therion-lang", {
-    provideDocumentFormattingEdits(
-      document: vscode.TextDocument
-    ): vscode.TextEdit[] {
-      const edits = [];
-      let indent = 0;
-      for (let i = 0; i < document.lineCount; i++) {
-        const text = document.lineAt(i).text.trim();
+  vscode.languages.registerDocumentFormattingEditProvider(
+    ["therion-lang", "therion-config-lang", "therion-2d-lang"],
+    {
+      provideDocumentFormattingEdits(
+        document: vscode.TextDocument
+      ): vscode.TextEdit[] {
+        const edits = [];
+        let indent = 0;
+        for (let i = 0; i < document.lineCount; i++) {
+          const text = document.lineAt(i).text.trim();
 
-        // Indendation
-        if (endTages.test(text)) indent--;
-        const newText = `${getConfig("format.indentCharacters").repeat(
-          indent
-        )}${text}`;
-        if (startTags.test(text)) indent++;
-        edits.push(vscode.TextEdit.replace(document.lineAt(i).range, newText));
+          // Indendation
+          if (endTages.test(text)) indent--;
+          const newText = `${getConfig("format.indentCharacters").repeat(
+            indent
+          )}${text}`;
+          if (startTags.test(text)) indent++;
+          edits.push(
+            vscode.TextEdit.replace(document.lineAt(i).range, newText)
+          );
 
-        // Special case for "code" which can be a single line or a block
-        if (code.test(text)) {
-          for (let j = i + 2; j < document.lineCount; j++) {
-            const nextText = document.lineAt(j).text.trim();
-            if (!nextText.length) continue;
-            if (nextText[0] !== "\\") {
-              indent++;
+          // Special case for "code" which can be a single line or a block
+          if (code.test(text)) {
+            for (let j = i + 2; j < document.lineCount; j++) {
+              const nextText = document.lineAt(j).text.trim();
+              if (!nextText.length) continue;
+              if (nextText[0] !== "\\") {
+                indent++;
+              }
+              break;
             }
-            break;
           }
         }
+        return edits;
       }
-      return edits;
     }
-  });
+  );
 
   // Template generator
 
