51740d3397c50d51403cc476b92898560e26f6b4
Rhys Tyers <rhystyers1@gmail.com>
2020-03-16 10:39:01 +0000

    Add generic open context menu item

--- src/open.ts
+++ src/open.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-
+import { platform } from "process";
 import { exec, getConfig } from "./util";
 
 export function activateOpen(context: vscode.ExtensionContext) {
@@ -9,4 +9,16 @@ export function activateOpen(context: vscode.ExtensionContext) {
       exec(`${getConfig("inkscapePath")} ${file.path}`);
     })
   );
+  if (getConfig("showOpen")) {
+    // Open Context Command
+    context.subscriptions.push(
+      vscode.commands.registerCommand("therion.context.open", file => {
+        if (platform === "linux") {
+          exec(`${"xdg-open"} ${file.path}`);
+        } else if (platform === "darwin") {
+          exec(`${"open"} ${file.path}`);
+        }
+      })
+    );
+  }
 }
