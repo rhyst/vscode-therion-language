aedc386bc8ced0bf032c65a08faa512d3df41199
Rhys Tyers <5313660+rhyst@users.noreply.github.com>
2020-03-16 09:57:04 +0000

    Remove starting whitespace when formatting

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,9 +1,8 @@
 import * as vscode from "vscode";
 import { getConfig } from "./util";
 
-const startTags = /^(scrap|survey|map|centreline|centerline|line|layout|lookup|def|begingroup)(\s|$)/;
-const endTages = /^(endscrap|endsurvey|endmap|endcentreline|endcenterline|endline|endlayout|endlookup|enddef(;)?|endgroup(;)?|endcode)(\s|$)/;
-const code = /^code\s/;
+const startTags = /^(scrap|survey|map|centreline|centerline|line|layout|lookup|def|begingroup|code$|source$)(\s|$)/;
+const endTages = /^(endscrap|endsurvey|endmap|endcentreline|endcenterline|endline|endlayout|endlookup|enddef(;)?|endgroup(;)?|endcode|endsource)(\s|$)/;
 
 export function activateFormatter() {
   // Formatter
@@ -15,8 +14,23 @@ export function activateFormatter() {
       ): vscode.TextEdit[] {
         const edits = [];
         let indent = 0;
+        let begin = true;
         for (let i = 0; i < document.lineCount; i++) {
           const text = document.lineAt(i).text.trim();
+
+          // Remove leading whitespace
+          if (begin && /^\s*$/.test(text)) {
+            edits.push(
+              vscode.TextEdit.delete(
+                new vscode.Range(
+                  document.lineAt(i).range.start,
+                  document.lineAt(i + 1).range.start
+                )
+              )
+            );
+          } else if (begin) {
+            begin = false;
+          }
 
           // Indendation
           if (endTages.test(text)) indent--;
@@ -27,18 +41,6 @@ export function activateFormatter() {
           edits.push(
             vscode.TextEdit.replace(document.lineAt(i).range, newText)
           );
-
-          // Special case for "code" which can be a single line or a block
-          if (code.test(text)) {
-            for (let j = i + 2; j < document.lineCount; j++) {
-              const nextText = document.lineAt(j).text.trim();
-              if (!nextText.length) continue;
-              if (nextText[0] !== "\\") {
-                indent++;
-              }
-              break;
-            }
-          }
         }
         return edits;
       }
