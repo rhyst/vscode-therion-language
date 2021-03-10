0c620d7d120d955617f986bfb3bbb6d91e0aefbc
Rhys Tyers <rhystyers1@gmail.com>
2021-03-10 21:18:13 +0000

    Handle WSL file paths, use  package for opening things

--- src/open.ts
+++ src/open.ts
@@ -1,24 +1,31 @@
 import * as vscode from "vscode";
-import { platform } from "process";
-import { exec, getConfig } from "./util";
+import { getConfig } from "./util";
+import path from "path";
+import open from "open";
 
 export function activateOpen(context: vscode.ExtensionContext) {
   // Open in Inkscape Context Command
   context.subscriptions.push(
-    vscode.commands.registerCommand("therion.context.openInInkscape", file => {
-      exec(`${getConfig("inkscapePath")} "${file.fsPath}"`);
+    vscode.commands.registerCommand("therion.context.openInInkscape", (file: vscode.Uri) => {
+      let filePath = file.fsPath;
+      if (vscode.env.remoteName === "wsl" && getConfig("wslName")) {
+        open(`\\\\wsl$\\${getConfig("wslName")}\\${file.fsPath.replace(path.posix.sep, path.win32.sep)}`, {
+          app: { name: "inkscape" },
+        });
+      } else {
+        open(filePath, { app: { name: "inkscape" } });
+      }
     })
   );
   if (getConfig("showOpen")) {
     // Open Context Command
     context.subscriptions.push(
-      vscode.commands.registerCommand("therion.context.open", file => {
-        if (platform === "linux") {
-          exec(`${"xdg-open"} "${file.fsPath}"`);
-        } else if (platform === "darwin") {
-          exec(`${"open"} "${file.fsPath}"`);
-        } else if (platform === "win32") {
-          exec(`"${file.fsPath}"`);
+      vscode.commands.registerCommand("therion.context.open", (file: vscode.Uri) => {
+        let filePath = file.fsPath;
+        if (vscode.env.remoteName === "wsl" && getConfig("wslName")) {
+          open(`\\\\wsl$\\${getConfig("wslName")}\\${file.fsPath.replace(path.posix.sep, path.win32.sep)}`);
+        } else {
+          open(filePath);
         }
       })
     );
